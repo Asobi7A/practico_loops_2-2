0ed46f040d1fca56d230a7e3935062b3c40ec057
Eito <eitoygeimatsui@gmail.com>
2025-04-06 00:01:24 -0400

    ejercicio3.cpp

---
+++ ejercicio3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string a; 
+    cout<<"ingrese 1 y 0s"<<endl;
+    cin>>a;
+for(int i = 0; i<a.length(); i++ )
+
+    cout<<"Om-nom-nom :p"<<endl;
+cout<<"se acabo la torta"<<endl;
+    return 0;
+}
