3b3d9b0da570f0b29d71d47ff0ab1b6fabde876a
Eito <eitoygeimatsui@gmail.com>
2025-04-05 23:54:59 -0400

    ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    
+
 string a;
 getline(cin, a);
 string word = "";
@@ -11,8 +11,11 @@ for (int i = 0; i<a.length() ; i++){
     if(a[i]!=' '){
     word+=a[i];
     }else if (a[i]==' '){
-    cout<<"["<<word<<"]"<<endl;;
+    cout<<"["<<word<<"]"<<endl;
     word = ""; }
+    if(i==a.length()-1){
+    cout<<"["<<word<<"]"<<endl;     
+    }
 }
     return 0;
 }
